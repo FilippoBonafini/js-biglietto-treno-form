e9bfcf986b9b193087571880be16744ff64ec062
Filippo Bonafini <filippo.bonafini@outlook.it>
2023-02-22 16:55:51 +0100

    Milestone 1 completato

---
+++ js/main.js
@@ -0,0 +1,43 @@
+'use strict'
+
+//Definiamo le variabili costanti:
+const priceKm = 0.21; //---------> Euro per ogni km percorso
+const discountUnder18 = 20; //---> Percentuale sconto under 18
+const discountOver65 = 40; //----> Percentuale sconto over 65
+
+//Definiamo le variabili contenenti parti del DOM:
+//bottoni
+const buttonConferm = document.getElementById("idBottoneConferma");
+const buttonReset = document.getElementById("idBottoneReset");
+//text-input
+let contentAge;
+let contentKm;
+let contentName;
+
+//Definiamo le variabili che prenderanno un valore una volta inseriti i dati
+let priceFull
+let finalPrice
+
+//Iniziamo un evento:
+buttonConferm.addEventListener("click",
+    function(){
+
+        //salviamo il conenuto degli input dentro le variabili prima inizializzate
+        contentAge = document.getElementById("idInputEta").value;
+        contentKm = document.getElementById("idInputKm").value;
+        priceFull = priceKm * Number(contentKm);
+
+        //Verifichiamo se l'utente può accedere a sconti e se si salviamo le variabili
+        if (Number(contentAge) < 18){
+            finalPrice = (priceFull-(discountUnder18 / 100)*priceFull).toFixed(2); //---> minorenni
+            console.log(finalPrice);
+        }
+        else if (Number(contentAge) >= 65){
+            finalPrice =(priceFull-(discountOver65 / 100)*priceFull).toFixed(2); //---> over
+            console.log(finalPrice);
+        }
+        else{
+            console.log(priceFull); //scriviamo direttamente il priceFull
+        }
+    }
+)
